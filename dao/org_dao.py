bdbc85eedcbd57edac5421cfb8e5f76ff89fd477
温清梦 <flidwala@gmail.com>
2024-02-13 13:35:55 +0800

    用tortoise api重写sql聚合查询，多表查询

--- dao/org_dao.py
+++ dao/org_dao.py
@@ -1,10 +1,8 @@
-from config import ENVIRONMENT
-from tortoise import Tortoise
+from entity.db_entity import *
+from tortoise.functions import Count
 
 
 async def get_orgs_members_cnt():
-    db = Tortoise.get_connection(ENVIRONMENT)
-    return await db.execute_query_dict("SELECT organization_id, title, COUNT(*) FROM member "
-                                       "JOIN organization ON member.organization_id = organization.id "
-                                       "GROUP BY organization_id;")
-
+    await Member.all().select_related("organization__id").\
+                 annotate(count=Count("id")).group_by("organization__id").\
+                 values("organization__id", "title", "count")
--- dao/record_dao.py
+++ dao/record_dao.py
@@ -1,27 +1,19 @@
-from tortoise import Tortoise
-from config import ENVIRONMENT
+from entity.db_entity import *
+from tortoise.expressions import Q
 
 
 async def get_courses_should_finish(mem_id):
-    db = Tortoise.get_connection(ENVIRONMENT)
-    return await db.execute_query_dict("SELECT course.id, type, title, start_datetime, end_datetime, cover, uri "
-                                       "FROM course WHERE start_datetime >= "
-                                       "(SELECT join_datetime FROM member WHERE id = %s)", [mem_id,])
+    join_datetime = (await Member.get(id=mem_id).values("join_datetime"))["join_datetime"]
+    return await Course.filter(start_datetime__lte=join_datetime).\
+                        values("id, type, title, start_datetime, end_datetime, cover, uri")
 
 
 async def get_courses_finished(mem_id):
-    db = Tortoise.get_connection(ENVIRONMENT)
-    return await db.execute_query_dict("SELECT course.id, type, title, start_datetime, end_datetime, cover, uri "
-                                       "FROM course "
-                                       "JOIN membercourse ON membercourse.course_id = course.id "
-                                       "WHERE member_id = %s", [mem_id,])
+    return await Course.filter(finish_member__member_id=mem_id).prefetch_related("finish_member__course_id").\
+                        values("id, type, title, start_datetime, end_datetime, cover, uri")
 
 
 async def get_org_records(org_id, course_id):
-    db = Tortoise.get_connection(ENVIRONMENT)
-    return await db.execute_query_dict("SELECT * FROM membercourse WHERE member_id in "
-                                       "(SELECT id FROM member WHERE organization_id = %s)"
-                                       "AND course_id = %s", [org_id, course_id,])
-
-
-
+    # List[Dict{"id":xxx}] -> List[xxx]
+    member_ids = [i["id"] for i in (await Member.filter(organization_id=org_id).values("id"))]
+    return await MemberCourse.filter(Q(member_id__in=member_ids) & Q(course_id=course_id))
